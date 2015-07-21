59797463400fa04a57bda20e13845ffc30b8f731
Artem Kostiuk <postatum@gmail.com>
2015-07-21 15:58:15 +0300

    Add ProcessableRealtionshipProperty imports

--- nefertari_sqla/__init__.py
+++ nefertari_sqla/__init__.py
@@ -1,7 +1,5 @@
 import logging
 
-from sqlalchemy.orm.properties import RelationshipProperty as RelationshipField
-
 from zope.dottedname.resolve import resolve
 
 from .documents import (
@@ -36,6 +34,9 @@ from .fields import (
     DictField,
     ListField,
 )
+
+from .fields import ProcessableRelationshipProperty as RelationshipField
+
 
 log = logging.getLogger(__name__)
 
