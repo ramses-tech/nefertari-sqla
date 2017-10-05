48d14eee452a3b4e9a4a7d3050f335d58ecc801c
Jonathan Stoikovitch <jstoiko@users.noreply.github.com>
2017-10-05 16:05:15 -0700

    Updated copyright

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -51,8 +51,8 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'nefertari-sqla'
-copyright = u'2015, Brandicted'
-author = u'Brandicted'
+copyright = u'Ramses Tech, Inc.'
+author = u'Ramses Tech'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -239,7 +239,7 @@ latex_elements = {
 #  author, documentclass [howto, manual, or own class]).
 latex_documents = [
     (master_doc, 'Nefertari.tex', u'Nefertari Documentation',
-     u'Brandicted', 'manual'),
+     author, 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
